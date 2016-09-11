450a175c3f74a66f362e18d87348e80ad1164ea6
Mateus Carvalho <mateus@mw8solucoes.com.br>
2016-09-11 01:06:29 -0300

    asynctask created for fetchdata

---
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -0,0 +1,118 @@
+package com.example.android.sunshine.app;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by mateus on 11/09/16.
+ */
+public class ForecastFragment extends Fragment {
+
+    private final String URL_WEATHER = "http://api.openweathermap.org/data/2.5/forecast/daily?q=418760000&mode=json&units=metric&cnt=7&APPID=";
+
+    public ForecastFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main,container,false);
+
+        String [] arrItens = {"Today - Sunny - 88/63", "Tomorrow - Foggy - 70/46","Weds - Cloudy - 72/63", "Thurs - Rainy - 64/51", "Fri - Froggy - 70/46"};
+
+        List<String> listItens = new ArrayList<String>(Arrays.asList(arrItens));
+
+        ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textview,listItens);
+
+        ListView listView = (ListView) view.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mAdapter);
+
+        return view;
+    }
+
+
+    private class FetchWeatherTask extends AsyncTask<String, String, String>{
+        // json response
+        String forecastJsonStr = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            try {
+                //the url
+                URL url = new URL(params[0]);
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    return null;
+                }
+
+                forecastJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                return null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+
+            return forecastJsonStr;
+        }
+
+        @Override
+        protected void onPostExecute(String string) {
+            super.onPostExecute(string);
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,24 +1,7 @@
 package com.example.android.sunshine.app;
 
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,81 +11,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceHolderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
 
-
-    public static class PlaceHolderFragment extends Fragment {
-
-        public PlaceHolderFragment(){}
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.fragment_main,container,false);
-
-            String [] arrItens = {"Today - Sunny - 88/63", "Tomorrow - Foggy - 70/46","Weds - Cloudy - 72/63", "Thurs - Rainy - 64/51", "Fri - Froggy - 70/46"};
-
-            List<String> listItens = new ArrayList<String>(Arrays.asList(arrItens));
-
-            ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textview,listItens);
-
-            ListView listView = (ListView) view.findViewById(R.id.listview_forecast);
-            listView.setAdapter(mAdapter);
-
-
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-
-            // json response
-            String forecastJsonStr = null;
-
-            try {
-                //the url
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=418760000&mode=json&units=metric&cnt=7");
-
-                // Create the request to OpenWeatherMap, and open the connection
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                // Read the input stream into a String
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    buffer.append(line + "\n");
-                }
-
-                if (buffer.length() == 0) {
-                    return null;
-                }
-
-                forecastJsonStr = buffer.toString();
-            } catch (IOException e) {
-                Log.e("PlaceholderFragment", "Error ", e);
-                return null;
-            } finally{
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e("PlaceholderFragment", "Error closing stream", e);
-                    }
-                }
-            }
-
-            return view;
-        }
-    }
 }

b370e586b5e6b38fce4ea5d965a1f2e14ea6674e
Mateus Carvalko <mateuscarvalho@MacBook-Pro-de-Mateus.local>
2016-10-28 00:10:34 -0300

    ForecastAdapter change for show the local data in view

--- app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
@@ -6,6 +6,7 @@ import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.android.sunshine.app.data.WeatherContract;
@@ -61,7 +62,24 @@ public class ForecastAdapter extends CursorAdapter {
         // our view is pretty simple here --- just a text view
         // we'll keep the UI functional with a simple (and slow!) binding.
 
-        TextView tv = (TextView)view;
-        tv.setText(convertCursorRowToUXFormat(cursor));
+       TextView dateTextView = (TextView) view.findViewById(R.id.list_item_date_textview);
+       TextView hightTemp = (TextView) view.findViewById(R.id.list_item_high_textview);
+       TextView lowTemp = (TextView) view.findViewById(R.id.list_item_low_textview);
+       TextView descTemp = (TextView) view.findViewById(R.id.list_item_forecast_textview);
+
+        boolean isMetric = Utility.isMetric(context);
+
+        hightTemp.setText(Utility.formatTemperature(cursor.getDouble(ForecastFragment.COL_WEATHER_MAX_TEMP),isMetric));
+        lowTemp.setText(Utility.formatTemperature(cursor.getDouble(ForecastFragment.COL_WEATHER_MIN_TEMP),isMetric));
+        descTemp.setText(cursor.getString(ForecastFragment.COL_WEATHER_DESC));
+        dateTextView.setText(Utility.getFriendlyDayString(context,cursor.getLong(ForecastFragment.COL_WEATHER_DATE)));
+
+        // Read weather icon ID from cursor
+        int weatherId = cursor.getInt(ForecastFragment.COL_WEATHER_ID);
+        // Use placeholder image for now
+        ImageView iconView = (ImageView) view.findViewById(R.id.list_item_icon);
+        iconView.setImageResource(R.mipmap.ic_launcher);
+
+
     }
 }

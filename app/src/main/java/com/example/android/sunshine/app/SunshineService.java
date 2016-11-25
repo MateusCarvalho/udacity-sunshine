fc614b2a926bebd48e054a5b9fd0d6c06b73c584
Mateus Carvalko <mateuscarvalho@MacBook-Pro-de-Mateus.local>
2016-11-25 08:18:07 -0300

    alarm mananger created

--- app/src/main/java/com/example/android/sunshine/app/SunshineService.java
+++ app/src/main/java/com/example/android/sunshine/app/SunshineService.java
@@ -2,8 +2,10 @@ package com.example.android.sunshine.app;
 
 import android.app.IntentService;
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -325,4 +327,12 @@ public class SunshineService extends IntentService{
         return locationId;
     }
 
+    static public class AlarmReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Intent sendIntent = new Intent(context, SunshineService.class);
+            sendIntent.putExtra(LOCATION_QUERY_EXTRA,intent.getStringExtra(LOCATION_QUERY_EXTRA));
+            context.startService(sendIntent);
+        }
+    }
 }
